7ddb9a74c9e8118254ebd81301dc45884e0b71c2
tlqtldh <tjdsmd@yahoo.com>
2017-10-15 16:57:17 +0900

    save

--- B_parser.py
+++ B_parser.py
@@ -1,4 +1,6 @@
 import re
+import numpy as np 
+from matplotlib import pyplot as plt
 
 path = 'D:/git/python/data/'
 
@@ -33,48 +35,77 @@ def pop_paser(filename, find_str1, find_str2, prime_num):
             time_sum = float(time_num) 
     return result    
 
+def mem_paser(filename, find_str1, find_str2, prime_num):
+     ## open the steam bmt result file
+    with open( 'D:/git/python/data/mem_bwt.txt', 'r') as content_file:
+        source = content_file.read()
+        content_file.close()
+    ## finding Triad using Regular expression operations
+    string = "Triad: \s+\d+.\d+"
+    re_search = re.compile(string)
+    text = re_search.findall(source, re.MULTILINE)
+    btw_data = 0
+    btw_num = 0
+    for items in text:
+        set_btw = items.split(':')
+        btw_data += float(set_btw[1].strip())
+        btw_num += 1
+    mem_btw = btw_num / btw_num
+    
+    with open( 'D:/git/python/data/mem-lat.txt', 'r') as content_file:
+        source = content_file.read()
+        content_file.close()
+    
+    string = "\d+[.]\d+"
+    re_search = re.compile(string)
+    text = re_search.findall(source, re.MULTILINE)
+    
+    mb = text[::2]
+    tim = text[1::2]
+    p = []
+    x = []
+    data_size = []
+    mem_lat = []
+    for i in range(4):
+        for j in range(37):
+            p.append([mb[j], tim[j]])
+        x.append(p)
+    
+    for i in range(37):
+        data_size.append(float(x[0][i][0]))
+        mem_lat.append(float(x[0][i][1]))
 
-#cpu_result = pop_paser('result.txt', 'Number of threads:', 'total time:', 20000)
-#print(cpu_result)
+    return mem_btw, data_size, mem_lat
 
- ## open the pop bmt result file
-with open( 'C:/Users/Lia/git/python/data/mem_bwt.txt', 'r') as content_file:
-    source = content_file.read()
-    content_file.close()
+def line_chart(x_value, y_value, title, x_label, y_label):
+    line_color = ['b', 'r', 'g', 'y']
+    #plt.subplot(2,1,1)
+    
+    plt.plot(x_value, y_value, line_color[0], label=title)
+    plt.xlabel( x_label, fontsize=10)
+    plt.ylabel( y_label, fontsize=10)
+    plt.title('CPU BMT', fontsize=20)
+    print(x_value)
+    plt.xticks(x_value, rotation=75)
+    plt.legend()
+    plt.grid()
+    plt.text(0, 0, 'Figure. CPU BMT', ha='center')
+    plt.savefig('bmt1.png')
+    plt.show()
 
-## finding threads and total time using Regular expression operations
-string = "Triad: \s+\d+.\d+"
-re_search = re.compile(string)
-text = re_search.findall(source, re.MULTILINE)
-btw_num = 0
-j = 0
-for items in text:
-    set_btw = items.split(':')
-    btw_num += float(set_btw[1].strip())
-    j += 1
+def bar_chart(x, y, title):
+    pass
 
-print(btw_num/j)
+def gen_report():
+    pass
 
-with open( 'C:/Users/Lia/git/python/data/mem-lat.txt', 'r') as content_file:
-    source = content_file.read()
-    content_file.close()
 
-string = "\d+[.]\d+"
-re_search = re.compile(string)
-text = re_search.findall(source, re.MULTILINE)
-
-mb = text[::2]
-tim = text[1::2]
-p = []
-x = []
-for i in range(4):
-    for j in range(37):
-        p.append([mb[j], tim[j]])
-    x.append(p)
-
-for i in range(37):
-    print(x[0][i][1])
 
 #print(mb)
 #print('='*30)
 #print(tim)
+
+cpu_result = pop_paser('result.txt', 'Number of threads:', 'total time:', 20000)
+print(cpu_result)
+
+#line_chart( data_size, mem_lat, 'CPU BMT', 'Array Size(MB)', 'Latency(ns)' )
