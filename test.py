7ea2c1a323b4ee1d32968b9e0fb5cd970ce91a45
tlqtldh <tjdsmd@yahoo.com>
2017-10-17 21:24:55 +0900

    save

--- test.py
+++ test.py
@@ -4,6 +4,12 @@ from matplotlib import pyplot as plt
 
 path = 'D:/git/python/results/'
 
+def isNumber(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
 
 def sto_paser(hostname):
     
@@ -18,19 +24,28 @@ def sto_paser(hostname):
     text = re_search.findall(source, re.MULTILINE)
 
     item_list = []
+    workload_list = []
 
     for items in text:
         if 'group' in items:
             item_list.append(items[:-8])
-        #elif 'read' in items or 'write' in items:
-        #    pattern = items[:-1]
-        #    print(pattern)
+        elif 'read' in items or 'write' in items:
+            item_list.append(items[:-1])
         elif 'bw' in items:
             bw = items.split('=')
             item_list.append(int(bw[1].strip()))
         elif 'iops' in items:
             iops = items.split('=')
             item_list.append(int(iops[1].strip()))
-    print(item_list)
+            workload_list.append(item_list)
+            item_list = []
+        
+    for j, w in enumerate(workload_list):
+        if isNumber(w[1]):
+            print(w[1])
+            workload_list[j].insert(0, workload_list[j-1][0])
+    
+    print(workload_list)
 
 sto_paser('BDP-BMT-MGMT01')
+
