c7c0b3e9fe2ea6d5df4aa29ad4b5f62cb8ad6158
tlqtldh <tjdsmd@yahoo.com>
2017-10-18 18:34:04 +0900

    save

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #import re
 #import numpy as np 
 #from matplotlib import pyplot as plt
-import bmt_parsing as bp 
-import bmt_report as br 
+import bmt_parser as bp 
+import bmt_chart as bc 
 
 
 cpu_result = bp.pop_paser('BDP-BMT-MGMT01', 'Number of threads:', 'total time:', 20000)
@@ -13,7 +13,7 @@ print(cpu_result)
 mem_result = bp.mem_paser('BDP-BMT-MGMT01', 'Triad: ')
 print(mem_result)
 #
-br.line_chart( cpu_result[0], cpu_result[1], 'CPU BMT', 'Thread', 'POPS' )
+bc.line_chart( cpu_result[0], cpu_result[1], 'CPU BMT', 'Thread', 'POPS' )
 #line_chart( mem_result[1], mem_result[2], 'Memory BMT', 'Array Size(MB)', 'Latency(ns)' )
 
 #if __name__ == '__main__':
