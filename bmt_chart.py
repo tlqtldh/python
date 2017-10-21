f07aa412cb3e806a3a3b99f33c8c5bcab29aa02d
tlqtldh <tjdsmd@yahoo.com>
2017-10-22 01:43:51 +0900

    save

--- bmt_chart.py
+++ bmt_chart.py
@@ -1,35 +1,36 @@
 import numpy as np 
 from matplotlib import pyplot as plt
 
-chart_color = ['b', 'r', 'g', 'y']
+chart_color = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'b', 'g', 'r', 'c', 'm', 'y', 'k']
 
-def single_line_chart(x_value, y_value, title, x_label, y_label):
+def single_line_chart(x_value, y_value, title, style, x_label, y_label):
     
     #plt.subplot(2,1,1)
     
-    plt.plot(x_value, y_value, chart_color[0], label=title)
+    plt.plot(x_value, y_value, chart_color[0], ls=style, label=title)
     plt.xlabel( x_label, fontsize=10)
     plt.ylabel( y_label, fontsize=10)
-    plt.title( 'CPU BMT', fontsize=20)
+    plt.title( 'SysBench CPU POPS BMT', fontsize=20)
     plt.xticks(x_value, rotation=75)
     plt.legend()
     plt.grid()
-    plt.text(0, 0, 'Figure. CPU BMT', ha='center')
-    plt.savefig('bmt1.png')
+    plt.savefig( title + 'cpubmt.png')
     plt.show()
 
-def multi_line_chart(values, title, x_label, y_label):
+def multi_line_chart(values, title, style, x_label, y_label):
+    
+    filename = 'cpubmt.png'
     
     for i in range(len(values)):
-        plt.plot(values[i][0], values[i][1], chart_color[i], label=title[i])
+        plt.plot(values[i][0], values[i][1], chart_color[i], ls=style[i], label=title[i])
+        filename = title[i] + filename
     
     plt.xlabel( x_label, fontsize=10)
     plt.ylabel( y_label, fontsize=10)
-    plt.title( 'CPU BMT', fontsize=20)
+    plt.title( 'LMBench Memory Latency BMT', fontsize=20)
     plt.legend()
     plt.grid()
-    plt.text(0, 0, 'Figure. CPU BMT', ha='center')
-    plt.savefig('multi_cpu_bmt.png')
+    plt.savefig( filename )
     plt.show()
 
 def bar_chart(data, title, x_label, y_label):
--- main.py
+++ main.py
@@ -7,17 +7,30 @@ import bmt_chart as bc
 
 
 cpu_result = [([1.0, 2.0, 4.0, 6.0, 8.0, 10.0, 12.0, 14.0, 16.0, 18.0, 20.0, 22.0, 24.0, 26.0, 28.0, 30.0, 32.0, 34.0, 36.0, 38.0, 40.0, 42.0], [892.4414908, 1663.69408, 3275.46264, 4629.8747, 5959.5818, 6268.808686, 8205.87652, 8716.254542, 9931.532652, 10920.39929, 11945.54772, 12980.9601, 14188, 15036, 16044, 17024, 17964, 18515, 19066, 19617, 20168, 20244 ])]
-cpu_title = ['2630v4','intel4108', 'intel5120']
+
 cpu_result.append(bp.pop_paser('intel4108', 'Number of threads:', 'total time:', 20000))
 cpu_result.append(bp.pop_paser('intel5120', 'Number of threads:', 'total time:', 20000))
+cpu_title = ['2630v4','intel4108', 'intel5120']
+cpu_style = ['--', '-', '-']
 #for i in range(len(cpu_result[0])):
 #    print(cpu_result[0][i], cpu_result[1][i])
-print(cpu_result)
-print(len(cpu_result))
-print(len(cpu_result[0][0]))
+#print(cpu_result)
+#print(len(cpu_result))
+#print(len(cpu_result[0][0]))
 
-#mem_result = bp.mem_paser('intel4108', 'Triad: ')
-#print(mem_result)
+mem_result = []
+mem_btw_result = []
+mem_lat_result = []
+
+mem_result.append(bp.mem_paser('intel4108', 'Triad: '))
+mem_result.append(bp.mem_paser('intel5120', 'Triad: '))
+
+for i in range(len(mem_result)):
+    mem_btw_result.append(mem_result[i][0])
+    mem_lat_result.append(mem_result[i][1:3])
+
+mem_title = ['intel4108', 'intel5120']
+mem_style = ['-', '-']
 
 # return(r_read_bw, r_read_iops, r_write_bw, r_write_ipos, r_m_read_bw, r_m_read_iops, r_m_write_bw, r_m_write_iops)
 #sto_result = bp.sto_paser('BDP-BMT-MGMT01')
@@ -25,8 +38,8 @@ print(len(cpu_result[0][0]))
 #block_size = [4, 8, 16, 32]
 
 #bc.single_line_chart( cpu_result[0][0], cpu_result[0][1], 'intel4108', 'Thread', 'POPS' )
-bc.multi_line_chart( cpu_result, cpu_title, 'Thread', 'POPS' )
-#bc.line_chart( mem_result[1], mem_result[2], 'Memory BMT', 'Array Size(MB)', 'Latency(ns)' )
+#bc.multi_line_chart( cpu_result, cpu_title, cpu_style, 'Thread', 'POPS' )
+bc.multi_line_chart( mem_lat_result, mem_title, mem_style, 'Array Size(MB)', 'Latency(ns)' )
 #bc.line_chart( block_size, sto_result[1], 'Random Read', 'Block Size(KB)', 'Bandwdith(GB/S)' )
 #bc.line_chart( block_size, sto_result[3], 'Random write', 'Block Size(KB)', 'Bandwdith(GB/S)' )
 
