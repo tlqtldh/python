ebcb80b39f775a359bf61fd7d0378e5703b70470
tlqtldh <tjdsmd@yahoo.com>
2017-11-12 16:52:05 +0900

    save

--- basic_deep/basic_nn.py
+++ basic_deep/basic_nn.py
@@ -1,9 +1,13 @@
 import sys, os
+from os import chdir
+chdir('D:\\git\\python\\basic_deep')
 sys.path.append(os.pardir)  # 부모 디렉터리의 파일을 가져올 수 있도록 설정
 import numpy as np
 import pickle
 from dataset.mnist import load_mnist
 from common.functions import sigmoid, softmax
+from PIL import Image
+
 
 #def identify_fun(x):
 #    return x
@@ -29,6 +33,13 @@ from common.functions import sigmoid, softmax
 #
 #print(Y)
 
+def softmax(data):
+    el_max = np.max(data)
+    exp_a = np.exp(data - el_max) # 오버 플로우 대책
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+    return y
+
 def get_data():
     (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, flatten=True, one_hot_label=False)
     return x_test, t_test
@@ -52,15 +63,16 @@ def predict(network, x):
     return y
 
 data =init_network()
-print(data)
+#print(data)
 
-#x, t = get_data()
-#network = init_network()
-#accuracy_cnt = 0
-#for i in range(len(x)):
-#    y = predict(network, x[i])
-#    p= np.argmax(y) # 확률이 가장 높은 원소의 인덱스를 얻는다.
-#    if p == t[i]:
-#        accuracy_cnt += 1
-#
-#print("Accuracy:" + str(float(accuracy_cnt) / len(x)))
+x, t = get_data()
+print(x.shape, t.shape)
+network = init_network()
+accuracy_cnt = 0
+for i in range(len(x)):
+    y = predict(network, x[i])
+    p= np.argmax(y) # 확률이 가장 높은 원소의 인덱스를 얻는다.
+    if p == t[i]:
+        accuracy_cnt += 1
+
+print("Accuracy:" + str(float(accuracy_cnt) / len(x)))
