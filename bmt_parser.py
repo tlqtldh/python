6a2c9a122dbc544ebbd509d671eb1707e563e078
tlqtldh <tjdsmd@yahoo.com>
2017-12-21 20:02:13 +0900

    save

--- bmt_parser.py
+++ bmt_parser.py
@@ -103,15 +103,13 @@ def sto_paser(hostname):
     s_write_bw = []
     r_read_bw = []
     r_write_bw = []
-    r_rwread_bw = []
-    r_rwwrite_bw = []
+    r_rw_bw = []
 
     s_read_iops = []
     s_write_iops = []
     r_read_iops = []
     r_write_iops = []
-    r_rwread_iops = []
-    r_rwwrite_iops = []
+    r_rw_iops = []
 
     for i in range(len(item_list)):
         if 'read' in item_list[i][2]:
@@ -120,19 +118,19 @@ def sto_paser(hostname):
         elif 'write' in item_list[i][2]:
             s_write_bw.append(int(item_list[i][47]))
             s_write_iops.append(int(item_list[i][48]))
-        elif 'k100' in item_list[i][2]:
+        if 'k100' in item_list[i][2]:
             r_read_bw.append(int(item_list[i][6]))
             r_read_iops.append(int(item_list[i][7]))
         elif 'k0' in item_list[i][2]:
             r_write_bw.append(int(item_list[i][47]))
             r_write_iops.append(int(item_list[i][48]))
         elif 'k7' in item_list[i][2]:
-            r_rwread_bw.append(int(item_list[i][6]))
-            r_rwread_iops.append(int(item_list[i][7]))
-            r_rwwrite_bw.append(int(item_list[i][47]))
-            r_rwwrite_iops.append(int(item_list[i][48]))
+            r_rw_bw.append(int(item_list[i][6]))
+            r_rw_iops.append(int(item_list[i][7]))
+#            r_rwwrite_bw.append(int(item_list[i][47]))
+#            r_rwwrite_iops.append(int(item_list[i][48]))
 
-    return(s_read_bw, s_write_bw, r_read_bw, r_write_bw, r_rwread_bw, r_rwwrite_bw, s_read_iops, s_write_iops, r_read_iops, r_write_iops, r_rwread_iops, r_rwwrite_iops)  
+    return(s_read_bw, s_write_bw, r_read_bw, r_write_bw, r_rw_bw, s_read_iops, s_write_iops, r_read_iops, r_write_iops, r_rw_iops)  
 
 def sto_paser_normal(hostname):
     
